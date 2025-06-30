b9964a472c987c35f9b970eeedefe9ca329c8b2a
badpx <badpxx@gmail.com>
2025-06-30 18:29:45 +0800

    Add build script to generate wasm static library.

---
+++ buildlame.sh
@@ -0,0 +1,20 @@
+##!/bin/bash
+
+if [ $# -eq 0 ]; then
+    echo "错误：请指定构建安装的目标目录" >&2  # 输出到标准错误
+    exit 1  # 返回非零状态码表示错误
+fi
+# 一次性清掉旧配置
+make distclean 2>/dev/null || true
+
+emconfigure ./configure \
+    --prefix=$1     \
+    --disable-frontend             \
+    --disable-shared --enable-static \
+    --disable-decoder --disable-gtktest \
+    --enable-nasm=no \
+    CFLAGS="-O3 -fPIC"
+
+# 编译 & 安装
+emmake make -j$(sysctl -n hw.logicalcpu)
+emmake make install
